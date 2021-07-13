8e6ed836753f7b528a1396f06d766cd7e02398df
dashtm4 <dashtm4@gmail.com>
2021-07-13 05:04:57 -0400

    feat: add packages & configs

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['./src/**/*.js', './public/index.html'],
+    darkMode: false,
+    theme: {
+        fontFamily: {
+            sans: ['Roboto', 'sans-serif'],
+            serif: ['"Roboto Slab"', 'serif'],
+            body: ['Roboto', 'sans-serif'],
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
